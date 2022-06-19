e49b2b8b166e14eef09f310b03e6b357a355db78
daikichidaze <dkch.yoshioka.t@gmail.com>
2022-06-19 15:44:11 +0700

    separate by cities

--- src/data-processing.py
+++ src/data-processing.py
@@ -1,7 +1,9 @@
+from operator import index
 import os
 import pandas as pd
 import numpy as np
 
+from datetime import datetime
 from glob import glob
 
 
@@ -12,21 +14,44 @@ class MissingValueCorrection:
         df.columns = ['poi_id', 'poi_name', 'country_name',
                       'lat', 'long', 'mean', 'max', 'min', 'precipitation']
 
+        # Add date column
+        df['date'] = datetime.strptime(input_csv_path[-14:-4], '%Y-%m-%d')
+
         df = MissingValueCorrection.inplace_nulls(df)
         df.dropna(inplace=True)
-        df.to_csv(output_csv_path)
+        df.to_csv(output_csv_path, index=False)
+        print(f'output the file: {output_csv_path}')
+        return
 
     @staticmethod
-    def inplace_nulls(cls, df: pd.DataFrame) -> pd.DataFrame:
+    def inplace_nulls(df: pd.DataFrame) -> pd.DataFrame:
         df.replace('', np.nan, inplace=True)
         df.replace(' ', np.nan, inplace=True)
         return df
 
 
+class SeparateByCity:
+    @staticmethod
+    def data_processing(df: pd.DataFrame, poi_id: int, output_csv_path: str):
+        df_tmp = df[df['poi_id'] == poi_id]
+        df_tmp.to_csv(output_csv_path, index=False)
+        print(f'output the file: {output_csv_path}')
+        return
+
+
 input_foler = 'data/raw'
-output_folder = 'data/processed'
+missing_value_output_folder = 'data/processed'
+by_city_output_folder = 'data/bycity'
 
 if __name__ == '__main__':
     for p in glob(os.path.join(input_foler, '*csv')):
-        output_path = os.path.join(output_folder, p.split('/')[-1])
+        output_path = os.path.join(
+            missing_value_output_folder, p.split('/')[-1])
         MissingValueCorrection.data_processing(p, output_path)
+
+    df_all_period = pd.concat([pd.read_csv(p) for p in glob(os.path.join(missing_value_output_folder, '*csv'))],
+                              axis=0).reset_index(drop=True)
+
+    for poi_id in df_all_period['poi_id'].drop_duplicates():
+        output_path = os.path.join(by_city_output_folder, f'{poi_id}.csv')
+        SeparateByCity.data_processing(df_all_period, poi_id, output_path)
